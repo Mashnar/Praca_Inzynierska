75eb91754525019470f53543831b824cfb54b5b1
szymon.fr <szymon.fr@gmail.com>
2019-10-03 01:46:32 +0200

    Grid i początki dużych rzeczy.

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     * Jest to controller renderujący widok głownej strony
+     */
+    public function index(): Response
+    {
+        //Zwracam widok
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+
+
+}
