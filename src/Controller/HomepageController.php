354c6d8670447be5e7a2ee9fc09e9754ed398ccc
Mashnar <szymon.fr@gmail.com>
2019-10-16 16:19:53 +0200

    Próby ze scrapowaniem planów.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class HomepageController extends AbstractController
 {
@@ -20,6 +26,40 @@ class HomepageController extends AbstractController
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+    }
+
+    /**
+     *
+     * @Route("/scrap", name="scrap")
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function scrap()
+    {
+        $client = HttpClient::create();
+        try {
+            $response = $client->request('GET', 'http://wfi.uni.lodz.pl/plany/stacjonarne/informatyka/1-stopnia/');
+        } catch (TransportExceptionInterface $e) {
+            throw $e;
+        }
+
+
+        try {
+            $crawler = new Crawler($response->getContent());
+        } catch (ClientExceptionInterface $e) {
+        } catch (RedirectionExceptionInterface $e) {
+        } catch (ServerExceptionInterface $e) {
+        } catch (TransportExceptionInterface $e) {
+        }
+
+        $crawler->filter('a')->each(function (Crawler $node) {
+            echo '<br>';
+            //tutaj bysmy regexa walneli na wsyzstkie z pdf i bysmy mieli nazwy plików
+            echo $node->attr('href');
+        });
+
+
+        return new Response();
     }
 
 
