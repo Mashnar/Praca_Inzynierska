db3896ca9a1b6bcea146c78ecaffc5d5888627fb
szymon.fr <szymon.fr@gmail.com>
2019-11-24 23:55:51 +0100

    SQL

--- kiosk_device.sql
+++ kiosk_device.sql
@@ -1,2 +1,8 @@
 INSERT INTO kiosk.device (id, name, description, ip_address, active, created_at, updated_at)
-VALUES (4, 'SDS_TEMP_1', null, '192.168.0.101', 1, '2019-10-22 13:07:17', '2019-10-22 13:07:17');
+VALUES (8, 'SDS_TEMP_1', 'dwór', '192.168.0.103', 1, '2019-11-14 12:19:42', '2019-11-14 12:19:42');
+INSERT INTO kiosk.device (id, name, description, ip_address, active, created_at, updated_at)
+VALUES (9, 'SDS_TEMP_2', 'piwnica piec', '192.168.0.104', 1, '2019-11-14 12:21:25', '2019-11-14 12:21:25');
+INSERT INTO kiosk.device (id, name, description, ip_address, active, created_at, updated_at)
+VALUES (10, 'SDS_TEMP_4', 'moj pokoj', '192.168.0.105', 1, '2019-11-14 12:25:07', '2019-11-14 12:25:07');
+INSERT INTO kiosk.device (id, name, description, ip_address, active, created_at, updated_at)
+VALUES (11, 'SDS_TEMP_3', 'pokoj rodzicow', '192.168.0.107', 1, '2019-11-14 12:27:39', '2019-11-14 12:27:39');
