3389c9722ddcb8765dcf3d5cb9e3201c0afa68b8
szymon.fr <szymon.fr@gmail.com>
2020-01-01 21:15:33 +0100

    Gotowe.

--- public/header/clock.js
+++ public/header/clock.js
@@ -8,8 +8,8 @@ function updateTime() {
     var dateObj = new Date();
     var month = dateObj.getMonth() + 1; //months from 1-12
     var day = dateObj.getDate();
-
-    document.documentElement.style.setProperty('--timer-day', "'" + day + "." + month);
+    document.documentElement.style.setProperty('--timer-day', "'" + day + "." + month + "." + dateObj.getFullYear());
+    // document.documentElement.style.setProperty('--timer-day', "'" + day + "." + month + "." + dateObj.getFullYear());
     document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("H") + "'");
     document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
     document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
